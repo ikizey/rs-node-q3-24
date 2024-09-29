a501e36ff391d8ee436158439af34b01e69c2528
kizey <ikizey@icloud.com>
2024-09-29 21:32:44 +0400

    feat: implement files listing function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+import { promises as fs } from "node:fs";
+import path from "node:path";
+
 const list = async () => {
-    // Write your code here 
+    const folderPath = path.join("src", "fs", "files");
+    const errorMessage = "FS operation failed";
+
+    try {
+        const files = await fs.readdir(folderPath);
+        console.log(files);
+    } catch (error) {
+        throw new Error(errorMessage);
+    }
 };
 
 await list();
