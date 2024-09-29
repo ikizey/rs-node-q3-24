b75bc96975dd10aa58a2d3a66cb17069ceeb990f
kizey <ikizey@icloud.com>
2024-09-29 20:58:11 +0400

    feat: implement file renaming function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,27 @@
+import { promises as fs } from "node:fs";
+import path from "node:path";
+
 const rename = async () => {
-    // Write your code here 
+    const oldPath = path.join("src", "fs", "files", "wrongFilename.txt");
+    const newPath = path.join("src", "fs", "files", "properFilename.md");
+    const errorMessage = "FS operation failed";
+
+    try {
+        await fs.access(oldPath, fs.constants.F_OK);
+
+        try {
+            await fs.access(newPath, fs.constants.F_OK);
+            throw new Error(errorMessage);
+        } catch (error) {
+            if (error.code !== "ENOENT") {
+                throw new Error(errorMessage);
+            }
+        }
+
+        await fs.rename(oldPath, newPath);
+    } catch (error) {
+        throw new Error(errorMessage);
+    }
 };
 
 await rename();
