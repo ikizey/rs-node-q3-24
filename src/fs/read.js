b00cbc4bb248de090c043f4c5c4ce789bd1261fc
kizey <ikizey@icloud.com>
2024-09-29 21:35:37 +0400

    feat: implement file reading function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import { promises as fs } from "node:fs";
+import path from "node:path";
+
 const read = async () => {
-    // Write your code here 
+    const filePath = path.join("src", "fs", "files", "fileToRead.txt");
+    const errorMessage = "FS operation failed";
+
+    try {
+        const content = await fs.readFile(filePath, "utf8");
+        console.log(content);
+    } catch (error) {
+        throw new Error(errorMessage);
+    }
 };
 
 await read();
