a3fbf5a3d69aa6fd3ec7c0d27c96fa8164f1cc98
kizey <ikizey@icloud.com>
2024-09-29 21:28:44 +0400

    feat: implement file deletion function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
+import { promises as fs } from "node:fs";
+import path from "node:path";
+
 const remove = async () => {
-    // Write your code here 
+  const filePath = path.join("src", "fs", "files", "fileToRemove.txt");
+  const errorMessage = "FS operation failed";
+
+  try {
+    await fs.unlink(filePath);
+  } catch (error) {
+    throw new Error(errorMessage);
+  }
 };
 
 await remove();
