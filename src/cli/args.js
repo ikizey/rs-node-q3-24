f44433e6c4f1d24c13c4c45251246735834b93b4
kizey <ikizey@icloud.com>
2024-09-29 22:12:35 +0400

    feat: implement function to log arguments

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const prefix = "--";
+
+  for (let i = 0; i < args.length; i += 2) {
+    const argKey = args[i].replace(prefix, "");
+    const argValue = args[i + 1];
+    console.log(`${argKey} is ${argValue}`);
+  }
 };
 
 parseArgs();
