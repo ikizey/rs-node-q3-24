433608780a9c45843edc97c915b22d8a9ad0dfd4
kizey <ikizey@icloud.com>
2024-09-29 22:01:33 +0400

    feat: implement function to log RSS_-prefixed environment variables

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+  const prefix = "RSS_";
+
+  Object.entries(process.env).forEach(([key, value]) => {
+    if (key.startsWith(prefix)) {
+      console.log(`${key}=${value}`);
+    }
+  });
 };
 
 parseEnv();
