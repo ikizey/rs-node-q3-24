a614953aa30f6edf71373e64f74f04472aa1eb9f
kizey <ikizey@icloud.com>
2024-09-30 10:49:12 +0400

    feat: implement input to file writing function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import { createWriteStream } from "node:fs";
+import { join } from "node:path";
+import { pipeline } from "node:stream/promises";
+
 const write = async () => {
-    // Write your code here 
+  const filePath = join("src", "streams", "files", "fileToWrite.txt");
+  const errorMessage = "Stream writing failed";
+
+  try {
+    const writeableStream = createWriteStream(filePath, { encoding: "utf-8" });
+
+    await pipeline(process.stdin, writeableStream);
+  } catch (error) {
+    throw new Error(errorMessage);
+  }
 };
 
 await write();
