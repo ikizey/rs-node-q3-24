7f6b31565256956a38d96880d1c4a13b411b8821
kizey <ikizey@icloud.com>
2024-09-30 10:49:44 +0400

    feat: implement input transforming function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+import { Transform } from "node:stream";
+import { pipeline } from "node:stream/promises";
+
 const transform = async () => {
-    // Write your code here 
+  const transformReverse = new Transform({
+    transform(buffer, encoding, callback) {
+      const reversed =
+        buffer.toString("utf-8").split("").reverse().join("") + "\n\n";
+
+      callback(null, reversed);
+    },
+  });
+
+  await pipeline(process.stdin, transformReverse, process.stdout);
 };
 
 await transform();
