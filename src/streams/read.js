235a29235290d90bd1e90b2da5ddd86447d94012
kizey <ikizey@icloud.com>
2024-09-30 10:47:52 +0400

    feat: implement file reading function using stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,23 @@
+import { createReadStream } from "node:fs";
+import { join } from "node:path";
+
 const read = async () => {
-    // Write your code here 
+  const filePath = join("src", "streams", "files", "fileToRead.txt");
+  const errorMessage = "Stream reading failed";
+
+  try {
+    const readableStream = createReadStream(filePath, {
+      encoding: "utf-8",
+    });
+
+    readableStream
+      .on("end", () => {
+        process.stdout.write("\n");
+      })
+      .pipe(process.stdout);
+  } catch (error) {
+    throw new Error(errorMessage);
+  }
 };
 
 await read();
