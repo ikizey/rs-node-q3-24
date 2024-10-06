1d052820abf337a9be4e6e071cd83813279ad921
kizey <ikizey@icloud.com>
2024-10-07 00:00:02 +0400

    feat: add cd command

--- src/commands/execute.js
+++ src/commands/execute.js
@@ -1,12 +1,15 @@
 import { ls } from "./ls.js";
 import { printPrompt } from "../prompt/prompt.js";
 import { exit } from "./exit.js";
+import { cd } from "./cd.js";
 
 export const execute = async (data) => {
   const command = data.toString("utf-8").trim();
 
   if (command === ".exit") {
       exit();
+  } else if (command.startsWith("cd ")) {
+    await cd(command);
   } else if (command === "ls") {
       await ls();
   } else {
