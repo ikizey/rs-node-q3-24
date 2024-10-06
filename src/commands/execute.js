3f03f8f95d2717891675915c58fdd9476c829156
kizey <ikizey@icloud.com>
2024-10-07 00:51:06 +0400

    feat: add cat command

--- src/commands/execute.js
+++ src/commands/execute.js
@@ -3,6 +3,7 @@ import { printPrompt } from "../prompt/prompt.js";
 import { exit } from "./exit.js";
 import { printInputError } from "../messages/inputError.js";
 import { cd } from "./navigation/cd.js";
+import { cat } from "./files/cat.js";
 
 export const execute = async (data) => {
   const command = data.toString("utf-8").trim();
@@ -15,6 +16,8 @@ export const execute = async (data) => {
     await cd("cd ..");
   } else if (command === "ls") {
     await ls();
+  } else if (command.startsWith("cat ")) {
+    await cat(command);
   } else {
     printInputError();
   }
