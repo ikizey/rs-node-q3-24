4f5bdb10c402bc49459e4a858fe6067dbc0ed85b
kizey <ikizey@icloud.com>
2024-10-06 17:18:08 +0400

    feat: implement spawning child process function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,21 @@
+import { spawn } from "node:child_process";
+import path from "node:path";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const scriptPath = path.join("src", "cp", "files", "script.js");
+  const errorMessage = "Failed to spawn child process";
+
+  const argsArray = args || [];
+
+  try {
+    spawn("node", [scriptPath, ...argsArray], {
+      stdio: ["inherit", "inherit", "inherit", "ipc"],
+    }).on("spawn", () => {
+      console.log("type CLOSE to exit");
+    });
+  } catch (error) {
+    throw new Error(errorMessage);
+  }
 };
 
 // Put your arguments in function call to test this functionality
