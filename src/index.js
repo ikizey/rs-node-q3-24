30fb7c6aceeb0c842bf54e20b7625fd113a1ee02
kizey <ikizey@icloud.com>
2024-10-06 21:51:32 +0400

    feat: print prompt on the start

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import printWelcomeMessage from "./welcome/welcome.js";
+import { printPrompt } from "./prompt/prompt.js";
 
 function main() {
   printWelcomeMessage();
+
+  printPrompt();
 }
 
 main();
