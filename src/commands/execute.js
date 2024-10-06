cf48b17201d808060258462b3e781170fbb82e92
kizey <ikizey@icloud.com>
2024-10-07 00:00:50 +0400

    feat: print input error on unknown command

--- src/commands/execute.js
+++ src/commands/execute.js
@@ -1,19 +1,20 @@
 import { ls } from "./ls.js";
 import { printPrompt } from "../prompt/prompt.js";
 import { exit } from "./exit.js";
+import { printInputError } from "../messages/inputError.js";
 import { cd } from "./cd.js";
 
 export const execute = async (data) => {
   const command = data.toString("utf-8").trim();
 
   if (command === ".exit") {
-      exit();
+    exit();
   } else if (command.startsWith("cd ")) {
     await cd(command);
   } else if (command === "ls") {
-      await ls();
+    await ls();
   } else {
-      console.log(command);
+    printInputError();
   }
 
   printPrompt();
