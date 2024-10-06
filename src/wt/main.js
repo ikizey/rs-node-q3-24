c89d214ab69eda860ecaf8d0dcf6d2903265e921
kizey <ikizey@icloud.com>
2024-10-06 16:25:05 +0400

    feat: implement worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,36 @@
+import { Worker } from "node:worker_threads";
+import { cpus } from "node:os";
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const performCalculations = async () => {
-    // Write your code here
+  const workerPath = join(__dirname, "worker.js");
+  const startNumber = 10;
+
+  const workerPromises = cpus().map((_, index) => {
+    const worker = new Worker(workerPath);
+
+    return new Promise((resolve) => {
+      worker.on("message", (result) => {
+        resolve({ status: "resolved", data: result });
+        worker.terminate();
+      });
+
+      worker.on("error", () => {
+        resolve({ status: "error", data: null });
+        worker.terminate();
+      });
+
+      worker.postMessage(startNumber + index);
+    });
+  });
+
+  const results = await Promise.all(workerPromises);
+
+  console.log(results);
 };
 
 await performCalculations();
