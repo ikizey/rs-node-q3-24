3309f5bc088befecb112c46403c4eff2bc244c52
kizey <ikizey@icloud.com>
2024-10-06 21:56:33 +0400

    feat: print exit message on ctrl+c

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+import * as app from "./commands/index.js";
 import printWelcomeMessage from "./welcome/welcome.js";
 import { printPrompt } from "./prompt/prompt.js";
 
 function main() {
   printWelcomeMessage();
+
+  process.on("SIGINT", () => {
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    app.exit();
+  });
 
   printPrompt();
 }
