a1af32d610d873db67f68ebc210c30c759f3fc45
kizey <ikizey@icloud.com>
2024-10-06 21:59:23 +0400

    feat: add exit command

--- src/commands/execute.js
+++ src/commands/execute.js
@@ -3,9 +3,14 @@ import { exit } from "./exit.js";
 
 export const execute = async (data) => {
   const command = data.toString("utf-8").trim();
+
   switch (command) {
+    case ".exit":
+      exit();
+      break;
     default:
       console.log(command);
   }
+
   printPrompt();
 };
