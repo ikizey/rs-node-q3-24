405a2e08c31e69af28ddf82bfd5f82c455ecccc1
kizey <ikizey@icloud.com>
2024-10-21 23:13:45 +0400

    feat: implement proper get all users route

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,16 @@
+import { db } from "./db/db";
 import { type IncomingMessage, type ServerResponse } from "node:http";
 
 export const router = (req: IncomingMessage, res: ServerResponse) => {
   if (req.method === "GET" && req.url === "/api/users") {
     res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ message: "Hello, World!" }));
+    const result = db.getAllUsers();
+    if (result.status === "success") {
+      res.end(JSON.stringify(result.users));
+    } else {
+      res.writeHead(500, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ error: "Internal Server Error" }));
+    }
   } else {
     res.writeHead(404, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ error: `${req.url} Not Found` }));
